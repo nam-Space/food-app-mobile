2f5b177a735a47729fafa72d19c3e220e0519ba6
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-07-25 13:17:13 +0700

    video 29

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
 
+  //data-type
+  //string
+  const [name, setName] = useState<string>("hoidanit");
+
+  //number
+  const [age, setAge] = useState<number>(30);
+
+  //null, undefined, boolean
+  const test = false;
+
+  //object, array
+  const [person, setPerson] = useState([6, 9]);
+
+
+
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.text}>test</Text>
+        <Text style={styles.text}>{name}</Text>
+        <Text style={styles.text}>{JSON.stringify(person)}</Text>
       </View>
       <Text style={styles.text}>Hello world with
         <Text style={styles.hoidanit}> hoidanit</Text>
@@ -28,8 +45,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: 60, color: "red"
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    fontSize: 60, color: "red",
+    paddingTop: 20,
+    paddingHorizontal: 20
+
   },
 });
