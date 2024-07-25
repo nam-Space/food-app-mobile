6ac9509d24237f83ae1084d6b775a505bffab251
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-07-25 12:56:14 +0700

    video 28

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Hello world with hoidanit it</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.text}>test</Text>
+      </View>
+      <Text style={styles.text}>Hello world with
+        <Text style={styles.hoidanit}> hoidanit</Text>
+      </Text>
     </View>
   );
 }
 
+//css-in-js /styled component
 const styles = StyleSheet.create({
+  hoidanit: {
+    color: "green",
+
+  },
+
+  text: {
+    fontSize: 30,
+    color: "red"
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontSize: 60, color: "red"
   },
 });
