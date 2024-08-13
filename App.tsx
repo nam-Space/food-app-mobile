722da3ee3a86421beec5e504c92c6fe29ce23466
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-08-13 15:46:39 +0700

    video 46

--- App.tsx
+++ App.tsx
@@ -1,25 +1,53 @@
 import {
+  Button,
   Text, View
 } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useNavigation } from '@react-navigation/native';
 
 export default function App() {
   const Stack = createNativeStackNavigator();
 
-  function HomeScreen() {
+  function HomeScreen(props: any) {
+    const navigation = props.navigation;
+
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
+        <View style={{ marginVertical: 10 }}>
+          <Button
+            onPress={() => navigation.navigate("hoidanit")}
+            title='Go to Detail' />
+        </View>
+        <View style={{ marginVertical: 10 }}>
+          <Button
+            onPress={() => navigation.navigate("hoidanit")}
+            title='Go user id = 1' />
+        </View>
+
+        <View style={{ marginVertical: 10 }}>
+          <Button
+            onPress={() => navigation.navigate("hoidanit")}
+            title='Go user id = 2' />
+        </View>
+
       </View>
     );
   }
 
   function DetailsScreen() {
+    const navigation: any = useNavigation();
+
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
+        <Text>user id = ??? </Text>
+        <Button
+          onPress={() => navigation.goBack()}
+          title='Go back Home' />
+
       </View>
     );
   }
@@ -28,7 +56,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="hoidanit" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
