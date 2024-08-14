637f969c1fd89d4405cc0a6c092c9894a747f6a1
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-08-14 17:52:11 +0700

    video 51

--- App.tsx
+++ App.tsx
@@ -10,124 +10,64 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from "@expo/vector-icons/Ionicons";
+import Home from './components/learn/home';
+import HomeDetail from './components/learn/home.detail';
+import Like from './components/learn/like';
+import LikeDetail from './components/learn/like.detail';
+import About from './components/learn/about';
+import ChangePassword from './components/learn/change.password';
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   const Drawer = createDrawerNavigator();
   const Tab = createBottomTabNavigator();
 
-  function HomeScreen(props: any) {
-    const navigation = props.navigation;
-
+  const TabApp = () => {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <View style={{ marginVertical: 10 }}>
-          <Button
-            onPress={() => navigation.navigate("hoidanit")}
-            title='Go to Detail' />
-        </View>
-        <View style={{ marginVertical: 10 }}>
-          <Button
-            onPress={() => navigation.navigate("hoidanit",
-              { userId: 1, name: "Eric" }
-            )}
-            title='Go user id = 1' />
-        </View>
-
-        <View style={{ marginVertical: 10 }}>
-          <Button
-            onPress={() => navigation.navigate("hoidanit", {
-              userId: 2, name: "Hỏi Dân IT"
-            })}
-            title='Go user id = 2' />
-        </View>
-
-      </View>
-    );
-  }
-
-  function DetailsScreen() {
-    const route: any = useRoute();
-    const navigation: any = useNavigation();
-
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Text>user id = {route?.params?.userId} </Text>
-        <Button
-          onPress={() => navigation.goBack()}
-          title='Go back Home' />
-
-      </View>
-    );
-  }
-
-  return (
-    <NavigationContainer>
-      {/* <Stack.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#f4511e',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}
+      <Tab.Navigator
       >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Like" component={Like} />
+
+      </Tab.Navigator>
+    )
+  }
+
+  const StackApp = () => {
+    return (
+      <Stack.Navigator>
         <Stack.Screen
           name="Home"
-          component={HomeScreen}
-          options={{ title: "Trang chủ" }}
+          component={TabApp}
+          options={{ title: "Trang chủ", headerShown: false }}
         />
         <Stack.Screen
-          name="hoidanit"
-          component={DetailsScreen}
+          name="HomeDetail"
+          component={HomeDetail}
           options={({ route }: { route: any }) => ({
             headerTitle: `Xem chi tiết ${route?.params?.userId ?? ""}`,
           })}
         />
-      </Stack.Navigator> */}
-      {/* <Drawer.Navigator initialRouteName='hoidanit'>
-        <Drawer.Screen name="Article" component={DetailsScreen} />
-
-        <Drawer.Screen
-          options={{
-            drawerLabel: "Trang chủ",
-            headerTitle: "Trang chủ"
-          }}
-          name="hoidanit"
-          component={HomeScreen}
-
+        <Stack.Screen
+          name="LikeDetail"
+          component={LikeDetail}
         />
-      </Drawer.Navigator> */}
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+      </Stack.Navigator>)
+  }
 
-            if (route.name === 'Home') {
-              iconName = focused
-                ? 'american-football'
-                : 'american-football-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'aperture' : 'aperture-outline';
-            }
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator >
+        <Drawer.Screen
+          name="StackApp" component={StackApp} />
+        <Drawer.Screen
+          name="About" component={About} />
+        <Drawer.Screen
+          name="ChangePassword"
+          component={ChangePassword}
+        />
+      </Drawer.Navigator>
 
-            // You can return any component that you like here!
-            return <Ionicons
-              name={iconName as any}
-              size={size} color={color}
-            />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-        })}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={DetailsScreen} />
-      </Tab.Navigator>
     </NavigationContainer>
   );
 }
