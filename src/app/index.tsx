b45028b716ec9660e698792d1c5c4d411e854cf7
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-08-16 15:29:51 +0700

    video 56

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,17 @@
 import { Text, View } from "react-native"
-
+import { Link } from 'expo-router';
 const AppRoot = () => {
     return (
         <View>
             <Text> 1 Hello world with expo router hoidanit</Text>
+
+            <Link href={"/hoidanit"}>
+                Go to hoidanit
+            </Link>
+
+            <Link href={"/like"}>
+                Go to like
+            </Link>
         </View>
     )
 }
--- src/app/like/index.tsx
+++ src/app/like/index.tsx
@@ -1,6 +1,7 @@
 import { useNavigation } from "@react-navigation/native";
 import { Button, Text, View } from "react-native"
 
+
 const Like = () => {
     const navigation: any = useNavigation();
 
