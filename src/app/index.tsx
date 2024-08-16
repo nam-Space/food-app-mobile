b516004f3c29c4abc8b1c5916bd7084db3310f05
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-08-16 14:44:28 +0700

    video 55

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+const AppRoot = () => {
+    return (
+        <View>
+            <Text> 1 Hello world with expo router hoidanit</Text>
+        </View>
+    )
+}
+
+export default AppRoot;
